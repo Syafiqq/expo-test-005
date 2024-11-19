4925468b9e29bec7b83dd2700da6b8364528438f
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-19 21:03:33 +0700

    W1.2: 001, project, register providers

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,9 +1,34 @@
-import {Stack} from "expo-router";
+import {SplashScreen, Stack} from "expo-router";
+import React, {useCallback, useEffect} from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   return (
-    <Stack>
-      <Stack.Screen name="index"/>
-    </Stack>
+    <Providers>
+      <Stack>
+        <Stack.Screen name="index"/>
+      </Stack>
+    </Providers>
   );
+}
+
+function Providers({children}: { children: React.ReactNode }) {
+  return (
+    <FirstTimeSetup>
+      {children}
+    </FirstTimeSetup>
+  )
+}
+
+function FirstTimeSetup({children}: { children: React.ReactNode }) {
+  const executor = useCallback(async () => {
+    await SplashScreen.hideAsync();
+  }, []);
+
+  useEffect(() => {
+    executor().then().catch();
+  }, [executor]);
+
+  return children;
 }
