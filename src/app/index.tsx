1599d3e777cfb1a06b54c32b11304766209b2e51
msyafiqq <muhammad.syafiq@geniebook.com>
2024-11-20 16:58:17 +0700

    --revert-later

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,5 @@
-import { Text, View } from "react-native";
+import {Env} from '@env';
+import {Text, View} from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +10,14 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Env.APP_ENV: {Env.APP_ENV}</Text>
+      <Text>Env.APP_NAME: {Env.APP_NAME}</Text>
+      <Text>Env.APP_DESCRIPTION: {Env.APP_DESCRIPTION}</Text>
+      <Text>Env.APP_SCHEME: {Env.APP_SCHEME}</Text>
+      <Text>Env.APP_IOS_BUNDLE_ID: {Env.APP_IOS_BUNDLE_ID}</Text>
+      <Text>Env.APP_ANDROID_PACKAGE_NAME: {Env.APP_ANDROID_PACKAGE_NAME}</Text>
+      <Text>Env.APP_VERSION: {Env.APP_VERSION}</Text>
+      <Text>Env.APP_BUILD_NUMBER: {Env.APP_BUILD_NUMBER}</Text>
     </View>
   );
 }
